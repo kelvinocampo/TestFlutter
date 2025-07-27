5a8431740313d2d4fc20eb634d4c09c647ef21d5
kelvinocampo <kevinocampooso@gmail.com>
2025-07-27 12:39:13 -0500

    add persistence of config language

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleProvider with ChangeNotifier {
-  Locale _locale = const Locale('es');
+  Locale _locale = const Locale('es'); // Idioma por defecto
 
   Locale get locale => _locale;
 
-  void setLocale(Locale newLocale) {
+  LocaleProvider() {
+    _loadLocaleFromPrefs();
+  }
+
+  /// Carga el idioma guardado desde SharedPreferences
+  Future<void> _loadLocaleFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final localeCode = prefs.getString('localeCode');
+
+    if (localeCode != null && localeCode.isNotEmpty) {
+      _locale = Locale(localeCode);
+      notifyListeners();
+    }
+  }
+
+  /// Cambia el idioma y lo guarda en SharedPreferences
+  Future<void> setLocale(Locale newLocale) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('localeCode', newLocale.languageCode);
     _locale = newLocale;
     notifyListeners();
   }
